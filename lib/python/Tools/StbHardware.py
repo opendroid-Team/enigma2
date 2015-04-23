2c83568918f0f26f5c8328dd920a8bbd23f9d87a
formiano <formianodicastellone@hotmail.it>
2015-04-23 19:16:53 +0200

    fix commit hours amiko alien

--- lib/python/Tools/StbHardware.py
+++ lib/python/Tools/StbHardware.py
@@ -37,10 +37,10 @@ def setFPWakeuptime(wutime):
 
 def setRTCoffset():
 	import time
-	if time.localtime().tm_isdst == 0:
-		forsleep = 7200+time.timezone
-	else:
-		forsleep = 3600-time.timezone
+		if time.localtime().tm_isdst == 0:
+			forsleep = 7200+time.timezone
+		else:
+			forsleep = 3600-time.timezone
 
 	t_local = time.localtime(int(time.time()))
 
