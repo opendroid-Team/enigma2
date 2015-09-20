8755bba9a9f12297b20cf94b532a2a537c29ab96
Opendroid <opendroid2014@gmail.com>
2015-09-20 17:58:23 +0200

    Update skin.py

--- skin.py
+++ skin.py
@@ -80,7 +80,7 @@ def skin_user_skinname():
 
 # example: loadSkin("nemesis_greenline/skin.xml")
 config.skin = ConfigSubsection()
-DEFAULT_SKIN = "Nowa/skin.xml"
+DEFAULT_SKIN = "oDreamy/skin.xml"
 if not fileExists(resolveFilename(SCOPE_SKIN, DEFAULT_SKIN)):
 	DEFAULT_SKIN = "skin.xml"
 config.skin.primary_skin = ConfigText(default=DEFAULT_SKIN)
