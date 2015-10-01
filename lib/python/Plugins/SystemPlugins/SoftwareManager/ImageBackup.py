b08c5f62d23657be5e221ec0ee2d8da79afb5ec9
gimsy <gimsy1@gmail.com>
2015-10-02 00:59:17 +0200

    Update ImageBackup version

--- lib/python/Plugins/SystemPlugins/SoftwareManager/ImageBackup.py
+++ lib/python/Plugins/SystemPlugins/SoftwareManager/ImageBackup.py
@@ -18,7 +18,7 @@ import commands
 import datetime
 from boxbranding import getBoxType, getMachineBrand, getMachineName, getDriverDate, getImageVersion, getImageBuild, getBrandOEM, getMachineBuild, getImageFolder, getMachineUBINIZE, getMachineMKUBIFS, getMachineMtdKernel, getMachineKernelFile, getMachineRootFile, getImageFileSystem
 
-VERSION = "Version 5.0 Opendroid"
+VERSION = "Version 5.2 Opendroid"
 
 HaveGZkernel = True
 if getBrandOEM() in ("fulan"):
