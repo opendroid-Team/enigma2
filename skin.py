7b681048cc39802cb55526fb31bce01f8d3d1a72
formiano <formianodicastellone@hotmail.it>
2016-11-13 07:02:21 +0100

    Update skin.py

--- skin.py
+++ skin.py
@@ -90,7 +90,7 @@ def skin_user_skinname():
 
 # example: loadSkin("nemesis_greenline/skin.xml")
 config.skin = ConfigSubsection()
-DEFAULT_SKIN = "oDreamy/skin.xml"
+DEFAULT_SKIN = "oDreamy-FHD/skin.xml"
 if not fileExists(resolveFilename(SCOPE_SKIN, DEFAULT_SKIN)):
 	DEFAULT_SKIN = "skin.xml"
 if not fileExists(resolveFilename(SCOPE_SKIN, DEFAULT_SKIN)):
