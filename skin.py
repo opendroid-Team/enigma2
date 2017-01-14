8edc22f15880242079004063af6e344cc008f881
gimsy <gimsy1@gmail.com>
2017-01-14 17:32:30 +0100

    revert last commit

--- skin.py
+++ skin.py
@@ -29,6 +29,10 @@ fonts = {
 }
 
 parameters = {}
+constant_widgets = {}
+variables = {}
+DEFAULT_SKIN = 'oDreamy-FHD/skin.xml'
+DEFAULT_DISPLAY_SKIN = 'skin_display.xml'
 
 def dump(x, i=0):
 	print " " * i + str(x)
