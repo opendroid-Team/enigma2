f6d6d8fd940ddef396a1695f8b2fbd46e7594161
formiano <formianodicastellone@hotmail.it>
2017-01-21 08:45:57 +0100

    test FHD

--- lib/gdi/gpixmap.h
+++ lib/gdi/gpixmap.h
@@ -225,8 +225,10 @@ public:
 		accelAlways = 1,
 	};
 
+	typedef void (*gPixmapDisposeCallback)(gPixmap* pixmap);
 	gPixmap(gUnmanagedSurface *surface);
 	gPixmap(eSize, int bpp, int accel = 0);
+	gPixmap(int width, int height, int bpp, gPixmapDisposeCallback on_dispose, int accel = accelAuto);
 
 	gUnmanagedSurface *surface;
 
@@ -236,7 +238,7 @@ public:
 	eSize size() const { return eSize(surface->x, surface->y); }
 
 private:
-	bool must_delete_surface;
+	gPixmapDisposeCallback on_dispose;
 
 	friend class gDC;
 	void fill(const gRegion &clip, const gColor &color);
