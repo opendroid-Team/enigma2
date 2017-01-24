77d9d7709405c8ef1851caf30997d42adcaeb3f1
gimsy <gimsy1@gmail.com>
2017-01-24 14:00:06 +0100

    Fixed compilation error imageBackup

--- lib/python/Plugins/SystemPlugins/SoftwareManager/ImageBackup.py
+++ lib/python/Plugins/SystemPlugins/SoftwareManager/ImageBackup.py
@@ -280,7 +280,7 @@ class ImageBackup(Screen):
 			system('mv %s/kernel_auto.bin %s/kernel_auto.bin' %(self.WORKDIR, self.MAINDEST))
 		else:
 			system('mv %s/vmlinux.gz %s/%s' %(self.WORKDIR, self.MAINDEST, self.KERNELBIN))
-		elif self.MODEL in ("vuultimo4k","vusolo4k", "vuduo2", "vusolo2", "vusolo", "vuduo", "vuultimo", "vuuno"):
+		if self.MODEL in ("vuultimo4k","vusolo4k", "vuduo2", "vusolo2", "vusolo", "vuduo", "vuultimo", "vuuno"):
 			cmdlist.append('echo "This file forces a reboot after the update." > %s/reboot.update' %self.MAINDEST)
 		elif self.MODEL in ("vuzero" , "vusolose", "vuuno4k"):
 			cmdlist.append('echo "This file forces the update." > %s/force.update' %self.MAINDEST)
