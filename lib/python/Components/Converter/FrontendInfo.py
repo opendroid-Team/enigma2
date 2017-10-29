27968ec7182b503786bf35faa0ecac6f5df8f8ad
formiano <formianodicastellone@hotmail.it>
2017-10-29 11:14:21 +0100

    update dm 920

--- lib/python/Components/Converter/FrontendInfo.py
+++ lib/python/Components/Converter/FrontendInfo.py
@@ -12,7 +12,6 @@ class FrontendInfo(Converter, object):
 	SLOT_NUMBER = 5
 	TUNER_TYPE = 6
 	STRING = 7
-	USE_TUNERS_STRING = 8
 
 	def __init__(self, type):
 		Converter.__init__(self, type)
@@ -33,9 +32,6 @@ class FrontendInfo(Converter, object):
 			type = type.split(",")
 			self.space_for_tuners = len(type) > 1 and int(type[1]) or 10
 			self.space_for_tuners_with_spaces = len(type) > 2 and int(type[2]) or 6
-			self.show_all_non_link_tuners = True if len(type) <= 3 else type[3] == "True"
-		elif type == "USE_TUNERS_STRING":
-			self.type = self.USE_TUNERS_STRING
 		else:
 			self.type = self.LOCK
 
@@ -68,8 +64,8 @@ class FrontendInfo(Converter, object):
 					if n.slot == self.source.slot_number:
 						color = "\c0000??00"
 					elif self.source.tuner_mask & 1 << n.slot:
-						color = "\c00????00"
-					elif len(nimmanager.nim_slots) <= self.space_for_tuners or self.show_all_non_link_tuners and not (n.isFBCLink() or n.internally_connectable):
+						color = "\c00??????"
+					elif len(nimmanager.nim_slots) <= self.space_for_tuners:
 						color = "\c007?7?7?"
 					else:
 						continue
@@ -77,23 +73,9 @@ class FrontendInfo(Converter, object):
 						string += " "
 					string += color + chr(ord("A")+n.slot)
 			return string
-		if self.type == self.USE_TUNERS_STRING:
-			string = ""
-			for n in nimmanager.nim_slots:
-				if n.type:
-					if n.slot == self.source.slot_number:
-						color = "\c0000??00"
-					elif self.source.tuner_mask & 1 << n.slot:
-						color = "\c00????00"
-					else:
-						continue
-					if string:
-						string += " "
-					string += color + chr(ord("A")+n.slot)
-			return string
 		if percent is None:
 			return "N/A"
-		return "%d %%" % (percent * 100 / 65535)
+		return "%d %%" % (percent * 100 / 65536)
 
 	@cached
 	def getBool(self):
@@ -140,5 +122,5 @@ class FrontendInfo(Converter, object):
 			num = self.source.slot_number
 			return num is None and -1 or num
 
-	range = 65535
+	range = 65536
 	value = property(getValue)
