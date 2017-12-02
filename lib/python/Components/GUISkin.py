ee9d5f3171e3f750a03f0c92e3aefc992857cae7
formiano <formianodicastellone@hotmail.it>
2017-12-02 18:00:48 +0100

    fix parameters yres 1080

--- lib/python/Components/GUISkin.py
+++ lib/python/Components/GUISkin.py
@@ -1,6 +1,7 @@
 from GUIComponent import GUIComponent
 from skin import applyAllAttributes
 from Tools.CList import CList
+from Components.config import config
 from Sources.StaticText import StaticText
 
 class GUISkin:
@@ -81,7 +82,7 @@ class GUISkin:
 
 	def applySkin(self):
 		z = 0
-		baseres = (720, 576) # FIXME: a skin might have set another resolution, which should be the base res
+		baseres = (1280, 720) # FIXME: a skin might have set another resolution, which should be the base res
 		idx = 0
 		skin_title_idx = -1
 		title = self.title
