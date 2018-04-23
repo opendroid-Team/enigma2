e3057c20d460c0f354063563effaa3cfb9af48fb
gimsy <gimsy1@gmail.com>
2018-04-24 00:27:55 +0200

    [scanSetup] fixed crash on manual scan on a single transponder

--- lib/python/Screens/ScanSetup.py
+++ lib/python/Screens/ScanSetup.py
@@ -1423,7 +1423,7 @@ class ScanSetup(ConfigListScreen, Screen, CableTransponderSearchSupport, Terrest
 		if not answer or self.scan_nims.value == "":
 			return
 		tlist = []
-		flags = None
+		flags = 0
 		removeAll = True
 		action = START_SCAN
 		index_to_scan = int(self.scan_nims.value)
