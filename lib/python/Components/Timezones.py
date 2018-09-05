b412ed56b23dbb3d069a2110bac8e4ad6db26324
Opendroid <opendroid2013@gmail.com>
2018-09-06 00:47:49 +0200

    Update Timezones.py

--- lib/python/Components/Timezones.py
+++ lib/python/Components/Timezones.py
@@ -139,7 +139,7 @@ class Timezones:
 		return sorttzChoices((tzname, self.getUserFriendlyTZName(area, tzname)) for tzname in self.timezones[area] if self.getUserFriendlyTZName(area, tzname))
 
 	default_for_area = {
-		'Europe': 'Rome,
+		'Europe': 'Rome'
 		'Generic': 'UTC',
 	}
 	def getTimezoneDefault(self, area=None, choices=None):
