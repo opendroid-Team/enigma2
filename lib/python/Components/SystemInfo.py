5c73a2ac6f49d7d899992de485ae5e816473696e
formiano <formianodicastellone@hotmail.it>
2018-09-16 08:41:06 +0200

    added getDisplayType

--- lib/python/Components/SystemInfo.py
+++ lib/python/Components/SystemInfo.py
@@ -2,7 +2,7 @@ from os import path
 from enigma import eDVBResourceManager, Misc_Options
 from Tools.Directories import fileExists, fileCheck
 from Tools.HardwareInfo import HardwareInfo
-from boxbranding import getBoxType, getMachineBuild, getBrandOEM
+from boxbranding import getBoxType, getMachineBuild, getDisplayType, getHaveRCA, getHaveDVI, getHaveYUV, getHaveSCART
 
 SystemInfo = { }
 
@@ -100,3 +100,5 @@ SystemInfo["ForceToneBurstChanged"] = fileCheck("/proc/stb/frontend/fbc/force_to
 SystemInfo["USETunersetup"] = SystemInfo["ForceLNBPowerChanged"] or SystemInfo["ForceToneBurstChanged"]
 SystemInfo["XcoreVFD"] = getMachineBuild() in ('xc7346','xc7439') 
 SystemInfo["HDMIin"] = getMachineBuild() in ('inihdp', 'hd2400', 'et10000', 'dm7080', 'dm820', 'dm900', 'dm920', 'vuultimo4k', 'et13000', 'sf5008', 'vuuno4kse') or getBoxType() in ('spycat4k','spycat4kcombo','gbquad4k')
+SystemInfo["HaveRCA"] = getHaveRCA() in ('True')
+SystemInfo["HaveDVI"] = getHaveDVI() in ('True')
