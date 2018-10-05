6666b6e4fde327cd9a000ac606c79ae872db59f7
gimsy <gimsy1@gmail.com>
2018-10-05 19:26:19 +0200

    [About.py] fix identation UnboundLocalError:local variable 'mark'

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -275,18 +275,18 @@ class About(Screen):
 			except:
 				tempinfo = ""
 		elif path.exists('/proc/hisi/msp/pm_cpu'):
-				try:
-					for line in open('/proc/hisi/msp/pm_cpu').readlines():
-						line = [x.strip() for x in line.strip().split(":")]
-				        if line[0] in ("Tsensor"):
-					        temp = line[1].split("=")
-					        temp = line[1].split(" ")
-					        tempinfo = temp[2]
-				except:
-					tempinfo = ""
-				if tempinfo and int(tempinfo.replace('\n', '')) > 0:
-					mark = str('\xc2\xb0')
-				AboutText += _("Processor Temperature:\t%s") % tempinfo.replace('\n', '').replace(' ','') + mark + "C\n"
+			try:
+				for line in open('/proc/hisi/msp/pm_cpu').readlines():
+					line = [x.strip() for x in line.strip().split(":")]
+					if line[0] in ("Tsensor"):
+						temp = line[1].split("=")
+						temp = line[1].split(" ")
+						tempinfo = temp[2]
+			except:
+				tempinfo = ""
+		if tempinfo and int(tempinfo.replace('\n', '')) > 0:
+			mark = str('\xc2\xb0')
+			AboutText += _("Processor Temperature:\t%s") % tempinfo.replace('\n', '').replace(' ','') + mark + "C\n"
 		AboutLcdText = AboutText.replace('\t', ' ')
 
 		self["HDDHeader"] = StaticText(_("Detected HDD:"))
