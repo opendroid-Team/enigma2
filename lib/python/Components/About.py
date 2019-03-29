bfa3a945a1b62b8ad7dc6341e2472d48de2a48a9
formiano <formianodicastellone@hotmail.it>
2019-03-29 08:17:41 +0100

    [vuduo4k] fix ChipSetString

--- lib/python/Components/About.py
+++ lib/python/Components/About.py
@@ -77,7 +77,9 @@ def getChipSetString():
 			return "unavailable"
 
 def getCPUSpeedString():
-	if getMachineBuild() in ('vusolo4k','vuultimo4k', 'vuzero4k'):
+	if getMachineBuild() in ('u41'):
+		return "1,0 GHz"
+	elif getMachineBuild() in ('dags72604','vusolo4k','vuultimo4k', 'vuzero4k'):
 		return "1,5 GHz"
 	elif getMachineBuild() in ('formuler1tc','formuler1', 'triplex', 'tiviaraplus'):
 		return "1,3 GHz"
@@ -122,7 +124,7 @@ def getCPUArch():
 def getCPUString():
 	if getMachineBuild() in ('vuduo4k','osmio4k','vuuno4kse','vuuno4k', 'vuultimo4k','vusolo4k', 'vuzero4k', 'hd51', 'hd52', 'sf4008', 'dm900','dm920', 'gb7252', 'dags7252', 'vs1500', 'et1x000', 'xc7439','h7','8100s','et13000','sf5008'):
 		return "Broadcom"
-	elif getMachineBuild() in ('u51','u52','u53','u54','u5','u5pvr','h9','h9combo','sf8008','i55plus'):
+	elif getMachineBuild() in ('gbmv200','u41','u51','u52','u53','u54','u55','u5','u5pvr','h9','h9combo','cc1','sf8008','hd60','hd61','i55plus','ustym4kpro','beyonwizv2','v8plus','multibox'):
 		return "Hisilicon"
 	else:
 		try:
@@ -151,8 +153,10 @@ def getCpuCoresString():
 			if len(splitted) > 1:
 				splitted[1] = splitted[1].replace('\n','')
 				if splitted[0].startswith("processor"):
-					if getMachineBuild() in ('u51','u52','u53','u54','vuultimo4k','u5','u5pvr','h9','h9combo','sf8008','i55plus','vuduo4k'):
+					if getMachineBuild() in ('gbmv200','u51','u52','u53','u54','u55','vuultimo4k','u5','u5pvr','h9','h9combo','alien5','cc1','sf8008','hd60','hd61','i55plus','ustym4kpro','beyonwizv2','v8plus','vuduo4k','multibox'):
 						cores = 4
+					elif getMachineBuild() in ('u41'):
+						cores = 2
 					elif int(splitted[1]) > 0:
 						cores = 2
 					else:
