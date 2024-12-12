08f514ed8d7a44d28518850568ef8b8d73705fac
formiano <formianodicastellone@hotmail.it>
2024-12-12 17:19:08 +0100

    if (local_style->is_set.use_vtiworkaround)

--- lib/gui/elistboxcontent.cpp
+++ lib/gui/elistboxcontent.cpp
@@ -399,7 +399,7 @@ void eListboxPythonStringContent::paint(gPainter &painter, eWindowStyle &style, 
 			{
 				text_offset += local_style->m_text_padding.topLeft();
 				// HACK OPD hat hier scheinbar einen Fehler und addiert den Textoffset zweimal auf, also machen wir das hier auch so
-				if (local_style->is_set.use_opd_workaround)
+				if (local_style->is_set.use_vti_workaround)
 					text_offset += local_style->m_text_padding.topLeft();
 
 				if (local_style->m_valign == eListboxStyle::alignTop)
